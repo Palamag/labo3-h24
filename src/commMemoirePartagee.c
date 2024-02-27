6019a79d7fde48240c99c4fa8b3168a7d232211c
Mathieu Faucher <mafau119@ulaval.ca>
2024-02-26 21:13:41 -0500

    debug de segFault

--- src/commMemoirePartagee.c
+++ src/commMemoirePartagee.c
@@ -18,21 +18,21 @@ int initMemoirePartageeLecteur(const char* identifiant, struct memPartage *zone)
         shm = shm_open(identifiant, O_RDWR, 0666);
     }
 
-    struct stat taille_Shm;
-    while (fstat(shm, &taille_Shm) < 0);
+    struct stat *taille_Shm = (struct stat*) malloc(sizeof(struct stat));
+    while (fstat(shm, taille_Shm) < 0);
 
-    unsigned char *ptr = (unsigned char*)mmap(NULL, taille_Shm.st_size, PROT_READ, MAP_SHARED, shm, 0);
+    unsigned char *ptr = (unsigned char*)mmap(NULL, taille_Shm->st_size, PROT_READ, MAP_SHARED, shm, 0);
 
     struct memPartageHeader *entete = (struct memPartageHeader*) ptr;
 
-    unsigned char *data = ptr + sizeof(&entete);
+    unsigned char *data = ptr + sizeof(struct memPartageHeader);
 
     zone->fd = shm;
     zone->header = entete;
-    zone->tailleDonnees = taille_Shm.st_size - sizeof(struct memPartageHeader);
+    zone->tailleDonnees = taille_Shm->st_size - sizeof(struct memPartageHeader);
     zone->data = data;
 
-    while(pthread_mutex_trylock(&zone->header->mutex) < 0) sched_yield();
+    pthread_mutex_trylock(&zone->header->mutex);
 
     return 0;
 }
